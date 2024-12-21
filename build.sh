fd518ad4eccf102708670eac9466df805b26bbac
Dušan Simić <dusan.simic1810@gmail.com>
2024-12-21 22:43:34 +0100

    feat: Cleanup and add hashicorp packages

--- build.sh
+++ build.sh
@@ -2,32 +2,21 @@
 
 set -ouex pipefail
 
-function log_info() {
-  echo "[INFO] $1"
-}
-
 RELEASE="$(rpm -E %fedora)"
 
 ### RPMFusion
 
-log_info "Add RPMFusion free and non-free"
 rpm-ostree install \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-41.noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-41.noarch.rpm
-
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$RELEASE.noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$RELEASE.noarch.rpm
 
 ### Remove packages
-
-log_info "Remove packages"
 
 rpm-ostree override remove \
   gnome-software \
   gnome-tour
 
-
 ### Install packages
-
-log_info "Install console tools"
 
 rpm-ostree install \
   eza \
@@ -39,18 +28,12 @@ rpm-ostree install \
   conda \
   ansible
 
-
-log_info "Install docker"
-
 rpm-ostree install \
   containerd.io \
   docker-buildx-plugin \
   docker-ce \
   docker-ce-cli \
   docker-compose-plugin
-
-
-log_info "Install misc. packages"
 
 rpm-ostree install \
   fira-code-fonts \
@@ -60,31 +43,24 @@ rpm-ostree install \
   sushi \
   tlp
 
-
-log_info "Install VirtualBox"
-
 rpm-ostree install \
   VirtualBox \
   VirtualBox-extpack
 
-
-log_info "Install additional file managers"
+rpm-ostree install \
+  gnome-commander
 
 rpm-ostree install \
-  gnome-commander \
-  krusader
+  terraform \
+  packer \
+  vagrant
 
 ### Exchange
-
-log_info "Exchange packages"
 
 rpm-ostree override \
   remove nano-default-editor \
   --install vim-default-editor
 
-
 ### Enable daemons
-
-log_info "Enable daemons"
 
 systemctl enable docker.service
