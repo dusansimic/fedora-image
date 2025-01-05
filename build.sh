44fee41546e2e458d9f9a46156a0511a29ea7d6b
Dušan Simić <dusan.simic1810@gmail.com>
2025-01-05 13:17:11 +0100

    feat: Use packages.jsonc for package management

--- build.sh
+++ build.sh
@@ -10,58 +10,16 @@ rpm-ostree install \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$RELEASE.noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$RELEASE.noarch.rpm
 
-### Remove packages
-
-rpm-ostree override remove \
-  gnome-tour
-
-### Install packages
 
 rpm-ostree install \
   /tmp/akmods-rpms/kmod-VirtualBox*.rpm
 
-rpm-ostree install \
-  eza \
-  fish \
-  gh \
-  git-lfs \
-  gitflow \
-  mosh \
-  conda \
-  ansible
-
-rpm-ostree install \
-  containerd.io \
-  docker-buildx-plugin \
-  docker-ce \
-  docker-ce-cli \
-  docker-compose-plugin
-
-rpm-ostree install \
-  fira-code-fonts \
-  gnome-backgrounds-extras \
-  gnome-console \
-  NetworkManager-sstp \
-  sushi \
-  tlp
-
-rpm-ostree install \
-  VirtualBox \
-  VirtualBox-extpack
-
-rpm-ostree install \
-  gnome-commander
+/tmp/packages.sh
 
 # rpm-ostree install \
 #   terraform \
 #   packer \
 #   vagrant
-
-### Exchange
-
-rpm-ostree override \
-  remove nano-default-editor \
-  --install vim-default-editor
 
 ### Enable daemons
 
