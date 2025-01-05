08f0ce619ab04959946b8f45cf52fcd5a6e18b8d
Dušan Simić <dusan.simic1810@gmail.com>
2025-01-05 15:44:49 +0100

    fix: Packages script json filename

--- packages.sh
+++ packages.sh
@@ -4,11 +4,11 @@ set -ouex pipefail
 
 # build list of all packages requested for inclusion
 INCLUDED_PACKAGES=($(jq -r "[(.all.include | (select(.all != null).all)[])] \
-                    | sort | unique[]" /tmp/packages.jsonc))
+                    | sort | unique[]" /tmp/packages.json))
 
 # build list of all packages requested for exclusion
 EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (select(.all != null).all)[])] \
-                    | sort | unique[]" /tmp/packages.jsonc))
+                    | sort | unique[]" /tmp/packages.json))
 
 # store a list of RPMs installed on the image
 INSTALLED_EXCLUDED_PACKAGES=()
