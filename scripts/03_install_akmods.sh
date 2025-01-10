82d76696aa023a20d8d98cd5121065d9d499ddb7
Dušan Simić <dusan.simic1810@gmail.com>
2025-01-10 12:44:16 +0100

    fix: Container tgz contents path

--- scripts/03_install_akmods.sh
+++ scripts/03_install_akmods.sh
@@ -4,8 +4,8 @@ set -ouex pipefail
 
 skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-extra:${VARIANT}-${RELEASE}-${KERNEL} dir:/tmp/akmods-extra
 AKMODS_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods-extra/manifest.json | cut -d : -f 2)
-tar -xzvf /tmp/akmods-extra/${AKMODS_TARGZ} -C /
-mv /tmp/akmods-rpms/* /tmp/akmods-extra/
+tar -xzvf /tmp/akmods-extra/${AKMODS_TARGZ} -C /tmp
+mv /tmp/rpms/kmods/* /tmp/akmods-extra/
 
 rpm-ostree install \
   /tmp/akmods-extra/*VirtualBox*.rpm
