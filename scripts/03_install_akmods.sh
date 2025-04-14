8cb5bc53fc680998ef94f7423980af0e61abeac9
Dušan Simić <dusan.simic1810@gmail.com>
2025-04-14 15:27:21 +0200

    fix: Skip VBox install

--- scripts/03_install_akmods.sh
+++ scripts/03_install_akmods.sh
@@ -2,13 +2,15 @@
 
 set -ouex pipefail
 
-# fetch kmods rpms
+# TODO: Skip VBox install for now
 
-skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-extra:${VARIANT}-${RELEASE}-${KERNEL} dir:/tmp/akmods-extra
-AKMODS_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods-extra/manifest.json | cut -d : -f 2)
-tar -xzvf /tmp/akmods-extra/${AKMODS_TARGZ} -C /tmp/
+# # fetch kmods rpms
 
-# install kmods
+# skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-extra:${VARIANT}-${RELEASE}-${KERNEL} dir:/tmp/akmods-extra
+# AKMODS_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods-extra/manifest.json | cut -d : -f 2)
+# tar -xzvf /tmp/akmods-extra/${AKMODS_TARGZ} -C /tmp/
 
-dnf5 -y install \
-  /tmp/rpms/kmods/*VirtualBox*.rpm
+# # install kmods
+
+# dnf5 -y install \
+#   /tmp/rpms/kmods/*VirtualBox*.rpm
