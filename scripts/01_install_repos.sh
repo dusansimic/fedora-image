b33123151e2c06a16c6c3acbf7dfa0f1fd58c004
Dušan Simić <dusan.simic1810@gmail.com>
2025-04-19 13:14:44 +0200

    fix: Add terra repo

--- scripts/01_install_repos.sh
+++ scripts/01_install_repos.sh
@@ -8,4 +8,5 @@ cp /ctx/repos/* /etc/yum.repos.d/
 
 dnf5 -y install \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$RELEASE.noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$RELEASE.noarch.rpm
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$RELEASE.noarch.rpm \
+  terra-release
