81d2b8394c33ce9019dabed30c5f8d26c56c7cef
Dušan Simić <dusan.simic1810@gmail.com>
2025-08-08 11:53:02 +0200

    fix: Download vagrant binary instead of rpm

--- build_files/packages/70_vagrant.sh
+++ build_files/packages/70_vagrant.sh
@@ -7,7 +7,15 @@ set -ouex pipefail
 VAGRANT_VERSION=$(curl -s https://releases.hashicorp.com/vagrant | htmlq --text 'html body ul li a' | sed 's~vagrant_~~' | sort -V | tail -n1)
 RELEASE_DIR_URL="https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/"
 VAGRANT_RELEASE=$(curl -s https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/ | htmlq --text 'html body ul li a' | grep 'x86_64.rpm' | sed 's~vagrant-~~;s~.x86.*~~' | sort -V | tail -n1)
-RPM_URL="https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant-${VAGRANT_RELEASE}.x86_64.rpm"
+ZIP_URL="https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_linux_amd64.zip"
 
-# Install Vagrant
-dnf5 -y install --allowerasing "${RPM_URL}"
+# Unzip into a temporary directory
+TMP_DIR=$(mktemp -d)
+curl -sL "${ZIP_URL}" -o "${TMP_DIR}/vagrant.zip"
+unzip -q "${TMP_DIR}/vagrant.zip" -d "${TMP_DIR}"
+
+# Move the vagrant binary to /usr/bin
+install -Dm755 "${TMP_DIR}/vagrant" -t /usr/bin
+
+# Cleanup temporary directory
+rm -rf "${TMP_DIR}"
