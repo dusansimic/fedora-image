81b7074d6d7010408b87722a10e452f5614d56cd
Dušan Simić <dusan.simic1810@gmail.com>
2025-08-08 10:36:21 +0200

    fix: Add htmlq

---
+++ build_files/packages/60_htmlq.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+API_URL="https://api.github.com/repos/mgdm/htmlq/releases/latest"
+TAR_URL=$(curl --fail --retry 5 --retry-delay 5 --retry-all-errors -sL "${API_URL}" | jq -r --arg arch_filter 'x86_64' '.assets | sort_by(.created_at) | reverse | .[] | select(.name|test("linux")) | .browser_download_url')
+
+# Download archive
+curl --fail --retry 5 --retry-delay 5 --retry-all-errors -L -o /tmp/htmlq.tar.gz "${TAR_URL}"
+
+# Extract and install
+mkdir -p /tmp/htmlq
+tar -xzf /tmp/htmlq.tar.gz -C /tmp/htmlq --strip-components 1
+chmod +x /tmp/htmlq/htmlq
+install -Dm755 /tmp/htmlq/htmlq -t /usr/local/bin
+
+# Cleanup
+rm -rf /tmp/htmlq /tmp/htmlq.tar.gz
