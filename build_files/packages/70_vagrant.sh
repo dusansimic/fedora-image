afff1933a4ef7ecdd6b5600e4c6e0b3af721ea76
Dušan Simić <dusan.simic1810@gmail.com>
2025-08-08 11:24:16 +0200

    fix: Vagrant rpm url

--- build_files/packages/70_vagrant.sh
+++ build_files/packages/70_vagrant.sh
@@ -7,7 +7,7 @@ set -ouex pipefail
 VAGRANT_VERSION=$(curl -s https://releases.hashicorp.com/vagrant | htmlq --text 'html body ul li a' | sed 's~vagrant_~~' | sort -V | tail -n1)
 RELEASE_DIR_URL="https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/"
 VAGRANT_RELEASE=$(curl -s https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/ | htmlq --text 'html body ul li a' | grep 'x86_64.rpm' | sed 's~vagrant-~~;s~.x86.*~~' | sort -V | tail -n1)
-RPM_URL="https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_RELEASE}_x86_64.rpm"
+RPM_URL="https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant-${VAGRANT_RELEASE}.x86_64.rpm"
 
 # Install Vagrant
 dnf5 -y install --allowerasing "${RPM_URL}"
