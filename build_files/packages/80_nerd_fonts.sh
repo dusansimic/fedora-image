89d5528932ffa133de5ad70406b5817c16ffc3e7
Dušan Simić <dusan.simic1810@gmail.com>
2025-11-21 16:05:42 +0100

    fix: Nerd fonts script

--- build_files/packages/80_nerd_fonts.sh
+++ build_files/packages/80_nerd_fonts.sh
@@ -5,6 +5,8 @@ set -ouex pipefail
 ## Helper functions
 
 download_and_unpack_font() {
+    fontname=$1
+    fontversion=$2
     curl -L -o /tmp/$fontname.tar.xz https://github.com/ryanoasis/nerd-fonts/releases/download/v$fontversion/$fontname.tar.xz
     mkdir /tmp/$fontname
     tar -xaf /tmp/$fontname.tar.xz --strip-components=1 -C /tmp/$fontname
