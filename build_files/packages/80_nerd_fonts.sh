cf56608cfd3f603e27f7f81d1a038a1525715fa4
Dušan Simić <dusan.simic1810@gmail.com>
2025-11-21 16:38:40 +0100

    fix: Nerd fonts script

--- build_files/packages/80_nerd_fonts.sh
+++ build_files/packages/80_nerd_fonts.sh
@@ -8,11 +8,11 @@ download_and_unpack_font() {
     fontname=$1
     fontversion=$2
     curl -L -o /tmp/$fontname.tar.xz https://github.com/ryanoasis/nerd-fonts/releases/download/v$fontversion/$fontname.tar.xz
-    mkdir -p /tmp/$fontname
+    mkdir /tmp/$fontname
     tar -xaf /tmp/$fontname.tar.xz --strip-components=1 -C /tmp/$fontname
     rm /tmp/$fontname.tar.xz
-    mkdir -p /usr/local/share/fonts/$fontname
-    mv /tmp/$fontname/*.ttf /usr/local/share/fonts/$fontname
+    mkdir /usr/share/fonts/${fontname}NerdFont
+    mv /tmp/$fontname/*.ttf /usr/share/fonts/${fontname}NerdFont
     rm -rf /tmp/$fontname
 }
 
@@ -22,6 +22,7 @@ FONT_VERSION=3.4.0
 
 ## Download and install fonts
 
+mkdir /usr/local/share/fonts
 download_and_unpack_font "CascadiaCode" $FONT_VERSION
 download_and_unpack_font "FiraCode" $FONT_VERSION
 download_and_unpack_font "AdwaitaMono" $FONT_VERSION
